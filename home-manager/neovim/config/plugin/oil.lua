48c095fd0afd2d528cfbcf1476d916fb010451f6
Remy <remyrfouquette@gmail.com>
2024-09-21 09:30:27 -0700

    add noice and change oil mappings

--- home-manager/neovim/config/plugin/oil.lua
+++ home-manager/neovim/config/plugin/oil.lua
@@ -4,6 +4,24 @@ require("oil").setup({
 		max_height = 40,
 	},
 	delete_to_trash = true,
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<leader>s"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+		["<leader>h"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" },
+		["<leader>t"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+		["<leader>p"] = "actions.preview",
+		["<leader>c"] = "actions.close",
+		["<leader>l"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" },
+		["gs"] = "actions.change_sort",
+		["gx"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+		["g\\"] = "actions.toggle_trash",
+	},
 })
 
 vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
