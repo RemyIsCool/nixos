4bb56733ad570174e30216555bb89828100a2e67
Remy <remyrfouquette@gmail.com>
2024-09-24 16:40:55 -0700

    update lualine neovim

--- home-manager/neovim/config/lua/plugins/lualine.lua
+++ home-manager/neovim/config/lua/plugins/lualine.lua
@@ -1,5 +1,18 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {}
+	opts = {
+		options = {
+			component_separators = { left = '|', right = '|' },
+			section_separators = { left = "", right = "" }
+		},
+		sections = {
+			lualine_a = { 'mode' },
+			lualine_b = { 'branch', 'diff', 'diagnostics' },
+			lualine_c = { 'buffers' },
+			lualine_x = { 'encoding', 'fileformat', 'filetype' },
+			lualine_y = { 'progress' },
+			lualine_z = { 'location' }
+		},
+	}
 };
