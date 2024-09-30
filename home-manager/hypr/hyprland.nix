698fe2bd78a050d8a9f291f4387f77f0bbd3da50
Remy <remyrfouquette@gmail.com>
2024-09-30 09:27:50 -0700

    fix

--- home-manager/hypr/hyprland.nix
+++ home-manager/hypr/hyprland.nix
@@ -122,8 +122,8 @@
 				"workspace 3, ^org.godotengine."
 			];
 			bindl = [
-				'', switch:on:"Lid Switch", exec, hyprctl keyword monitor "eDP-1, disable"''
-				'', switch:off:"Lid Switch", exec, hyprctl keyword monitor "eDP-1, 1920x1200, 0x0, 1"''
+				'', switch:on:Lid Switch, exec, hyprctl keyword monitor "eDP-1, disable"''
+				'', switch:off:Lid Switch, exec, hyprctl keyword monitor "eDP-1, 1920x1200, 0x0, 1"''
 			];
 
 			windowrulev2 = [ "suppressevent maximize, class:.*" ];
